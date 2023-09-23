798aa2eb642122c461fbd694e078d9b98046e111
Max Xue <maxxue1512@gmail.com>
2023-09-23 20:29:06 +1000

    adding articles and basic project frontend

--- frontend/app/article/[id]/page.tsx
+++ frontend/app/article/[id]/page.tsx
@@ -1,28 +1,57 @@
-import React from "react";
-import Markdown from "markdown-to-jsx";
+import React from 'react';
+import styles from './styles.module.css';
+import ReactMarkdown from 'react-markdown';
 
 async function getArticle(articleId: string) {
-  const res = await fetch(
-    `http://localhost:1337/api/articles/${articleId}?populate=*`,
-  );
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
+	const res = await fetch(
+		`http://localhost:1337/api/articles/${articleId}?populate=*`,
+		{ cache: 'no-store' }
+	);
+	if (!res.ok) {
+		throw new Error('Failed to fetch data');
+	}
+	return res.json();
 }
 
 export default async function ArticlePage({
-  params,
+	params,
 }: {
-  params: { id: string };
+	params: { id: string };
 }) {
-  const article = await getArticle(params.id);
-  return (
-    <div>
-      <h1>{article.data.attributes.Title}</h1>
-      <div>{JSON.stringify(article.data.attributes.tags)}</div>
-      <div>{article.data.attributes.updatedAt}</div>
-      <Markdown>{article.data.attributes.Content}</Markdown>
-    </div>
-  );
+	const article = await getArticle(params.id);
+	const monthNames = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December',
+	];
+	const getPublishedDate = () => {
+		const publishedDate = new Date(article.data.attributes.publishedAt);
+		return `${publishedDate.getDate()} ${
+			monthNames[publishedDate.getMonth()]
+		} ${publishedDate.getFullYear()}`;
+	};
+	return (
+		<div className={styles.page}>
+			<div className={styles.container}>
+				<h1 className={styles.title}>
+					{article.data.attributes.Title}
+				</h1>
+				<div className={styles.source}>
+					<span>John Smith</span>
+					<span>|</span>
+					<span>{getPublishedDate()}</span>
+				</div>
+				<ReactMarkdown>{article.data.attributes.Content}</ReactMarkdown>
+			</div>
+		</div>
+	);
 }
